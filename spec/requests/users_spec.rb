f76a822236c88b65e699caba4431647da2b26544
smoke-ck <2056sanya9snake@gmail.com>
2022-01-26 10:32:54 +0200

    styling andrefactoring

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  before do
-    @user = create(:user, email:"Bob@gmail.com",password:"password")
-  end
+  let(:user) { create(:user, email:"Bob@gmail.com",password:"password") }
+
   describe "GET #index" do
     it "returns http success" do
       get "/users/sign_in"
-      sign_in @user
+      sign_in user
       expect(response).to have_http_status(:success)
     end
   end
