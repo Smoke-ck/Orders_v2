89abb3bc010a41e26033b86ef0dfb0068aa74e5c
Smoke-ck <72155871+Smoke-ck@users.noreply.github.com>
2022-07-11 12:52:15 +0300

    Migrate from importmap to esbuild (#21)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Entry point for the build script in your package.json
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+
+import 'bootstrap/js/dist/dropdown'
+import 'bootstrap/js/dist/collapse'
 import "@hotwired/turbo-rails"
-import "controllers"
+import "./controllers/reset_form_controller"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
