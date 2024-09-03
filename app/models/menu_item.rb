36106b37dd4a876ad8c3db0bdbade867a65d84fc
Smoke-ck <2056sanya9snake@gmail.com>
2024-09-03 13:25:06 +0300

    Remove order items if menu item was deactivated (#35)

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -5,4 +5,12 @@ class MenuItem < ApplicationRecord
   has_many :order_items
   validates :title, presence: true, length: { minimum: 2 }
   validates :price, presence: true, format: { with: /\A\d{1,3}(\.\d{1,2})?\z/, message: "should be in the format is XXX.XX" }
+
+  after_update :remove_order_items, if: -> { active_previously_changed? && !active? }
+
+  private
+
+  def remove_order_items
+    order_items.destroy_all
+  end
 end
